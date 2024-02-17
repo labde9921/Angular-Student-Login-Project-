ef0d7d14d954abb3e64ac53df6a0af883b68a863
labde9921 <129748865+labde9921@users.noreply.github.com>
2024-02-17 11:52:02 +0530

    Student Login Project for Course
    I have made this Project for Student class

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  message : any;
+  constructor (private service : MarvallousService)
+  {}
+
+  ngOnInit(): void {
+    this.service.getBatches().subscribe(data=>{
+      this.message=data;
+    })
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MarvallousService} from './marvallous.service'
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
